9adb282f875939d16e197120276d3d846450afa5
Kristian Binau <kristian@binau.me>
2023-10-31 20:39:34 +0100

    Add Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/static";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel({
     speedInsights: {
-      enabled: true,
-    },
+      enabled: true
+    }
   }),
+  integrations: [tailwind()]
 });
