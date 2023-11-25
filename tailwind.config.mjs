3ff95a186cb4a8810fef38474106386262edd8a5
Kristian Binau <kristian@binau.me>
2023-11-25 02:45:15 +0100

    Add Card & Button components for more DRY code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'neu-pink': '#fb60f4',
+				'neu-red': '#fb6060',
+				'neu-cyan': '#60fbe1',
+				'neu-green': '#60fb7a',
+				'neu-yellow': '#fbee60',
+			},
+		},
 	},
 	plugins: [],
 };
